7d5ae76703888a36255c19161d0d25cf30aef8d3
Colton Someone <coltonmhagan@gmail.com>
2021-02-10 16:17:02 -0800

    added adjacentRooms to Room

--- Room.java
+++ Room.java
@@ -3,30 +3,27 @@ public class Room {
    private int currentShotCounters;
    private Role[] extraRoles;
    private SceneCard sceneCard;
+   private Room adjacentRooms;
    
-   public Room (int totalShotCounters,Role[] extraRoles, SceneCard sceneCard) {
-      
+   public Room (int totalShotCounters, Role[] extraRoles, SceneCard sceneCard) {
+   
    }
-   
    public void endRoom() {
    
    }
-   
    private void bonusPayment() {
    
    } 
-   
    public Role[] availableExtraRoles() {
       return null;
    }
-<<<<<<< HEAD
    public int getCurrentShotCounters () {
       return currentShotCounters;
    }
    private void removeScene() {
    
    }
-=======
-   
->>>>>>> 7e6bc0b08066f5a05934d92aa5d1db703c7e8554
+   private Room getAdjacentRooms () {
+      return adjacentRooms;
+   }
 }
