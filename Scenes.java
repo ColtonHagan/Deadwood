700fd67a392848b14de2ef1fdd9ae67e186a1258
Colton Someone <coltonmhagan@gmail.com>
2021-02-20 16:34:11 -0800

    adding flipping of a card

--- Scenes.java
+++ Scenes.java
@@ -9,6 +9,7 @@ public class Scenes {
       SceneCard randomCard = possibleScenes[rand.nextInt(40)];
       if(!randomCard.getUse()) {
          randomCard.setUse(true);
+         randomCard.setFlip(false);
          return randomCard;
       }
       return getRandomCard();
