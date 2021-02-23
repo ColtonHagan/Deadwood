321aaafe44cacfa757ba7a45710c976141ffb0b0
les5 <les5@wwu.edu>
2021-02-22 22:02:10 -0800

    Cleaned up main

--- Deadwood.java
+++ Deadwood.java
@@ -1,37 +1,6 @@
 public class Deadwood {
    public static void main(String args[]) throws Exception {
-      //int totalPlayers = Integer.parseInt(args[0]);
-
-      GameStateController control = new GameStateController(2);
+      GameStateController control = new GameStateController(Integer.parseInt(args[0]));
       control.setUpGame();
-      Room[] allRooms = control.getGameModel().getBoard().allRooms();
-
-
-/*
-      control.move(allRooms[9]);
-
-      //control.getCurrentRoom();
-
-      Role holden = allRooms[4].getSceneCard().getRoles()[0];
-      control.addRole(holden);
-      control.rehearse();
-      control.rehearse();
-      control.rehearse();
-      control.rehearse();
-      control.rehearse();
-      control.rehearse();
-      control.rehearse();
-      control.rehearse();
-      control.act();
-      control.updateMoney(2000);
-      control.upgradeRankCredits(3);
-      control.upgradeRankDollars(5);
-
-
-      control.endRoom(allRooms[4]);
-      control.endTurn();
-      control.endGame();
-
-       */
    }
 }
