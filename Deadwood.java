cafd45f683a4a8da06346bb146b29f7b89873979
Colton Someone <coltonmhagan@gmail.com>
2021-02-22 20:05:55 -0800

    merge fix for pull

--- Deadwood.java
+++ Deadwood.java
@@ -5,10 +5,10 @@ public class Deadwood {
 
       control.setUpGame();
       Room[] allRooms = control.getGameModel().getBoard().allRooms();
+      control.playGame();
 
 
-
-      //control.updateView();
+/*
       control.move(allRooms[9]);
 
       //control.getCurrentRoom();
@@ -32,5 +32,7 @@ public class Deadwood {
       control.endRoom(allRooms[4]);
       control.endTurn();
       control.endGame();
+
+       */
    }
 }
